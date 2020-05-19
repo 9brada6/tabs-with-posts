ef2fc5fe984e43c2e330efcfd8b9d75ec1f9065b
9brada6 <9brada6@gmail.com>
2020-05-19 11:43:31 +0300

    Fixed the author HTML

--- inc/Query_Setting/Author.php
+++ inc/Query_Setting/Author.php
@@ -8,6 +8,8 @@ class Author implements Query_Setting {
 
 	const AUTHORS_TYPE__SETTING_NAME = 'setting_type';
 
+	const AUTHORS_IDS__SETTING_NAME = 'authors';
+
 	/**
 	 * The name of the input and of the array key that stores the option of the query.
 	 *
@@ -48,21 +50,46 @@ class Author implements Query_Setting {
 	 * @return void
 	 */
 	public function display_setting( $current_setting ) {
+		$this->display_authors_select_type( $current_setting );
+		$this->display_selected_authors_list( $current_setting );
+		$this->display_add_authors_to_list( $current_setting );
+	}
+
+	/**
+	 * Display a select field which will tell what to do with the authors selected.
+	 *
+	 * @param array $current_setting
+	 * @return void
+	 */
+	protected function display_authors_select_type( $current_setting ) {
 		?>
 		<select
 			id="twrp-author-settings__select_type"
 			class="twrp-author-settings__select_type"
 			name="<?= esc_attr( $this->get_setting_name() . '[' . self::AUTHORS_TYPE__SETTING_NAME . ']' ); ?>"
+			value="<?= esc_attr( $current_setting[ self::AUTHORS_TYPE__SETTING_NAME ] ); ?>"
 		>
 			<option value="-1"><?= _x( 'Exclude selected authors', 'backend', 'twrp' ); ?></option>
 			<option value="1"><?= _x( 'Include selected authors', 'backend', 'twrp' ); ?></option>
 			<option value="2"><?= _x( 'Same author as the post', 'backend', 'twrp' ); ?></option>
 		</select>
+		<?php
+	}
 
+	/**
+	 * Display the list with the selected authors.
+	 *
+	 * @param array $current_setting
+	 * @return void
+	 */
+	protected function display_selected_authors_list( $current_setting ) {
+		?>
 		<div id="twrp-author-settings__js-authors-list" class="twrp-display-list">
-			<?php if ( isset( $current_setting['authors'] ) ) : ?>
-				<?php $authors_ids = explode( ';', $current_setting['authors'] ); ?>
-
+			<div class="twrp-display-list__empty-msg">
+				<?= _x( 'No authors selected. You can search for an author and click the button to add.', 'backend', 'twrp' ); ?>
+			</div>
+			<?php if ( isset( $current_setting[ self::AUTHORS_IDS__SETTING_NAME ] ) ) : ?>
+				<?php $authors_ids = explode( ';', $current_setting[ self::AUTHORS_IDS__SETTING_NAME ] ); ?>
 				<?php foreach ( $authors_ids as $author_id ) : ?>
 					<?php
 					$author_class = get_userdata( (int) $author_id );
@@ -71,24 +98,49 @@ class Author implements Query_Setting {
 					}
 					$author_display_name = $author_class->get( 'display_name' );
 
+					// The following HTML can also be generated in JS, so it will
+					// be need to be changed there as well.
 					?>
 					<div class="twrp-display-list__item twrp-author-settings__author-item" data-author-id="<?= esc_attr( $author_id ); ?>">
 						<div class="twrp-author-settings__author-item-name">
 							<?= esc_html( $author_display_name ); ?>
 						</div>
-						<button class="twrp-display-list__item-remove-btn twrp-author-settings__js-author-remove-btn" type="button"><span class="dashicons dashicons-no"></span></button>
+						<button class="twrp-display-list__item-remove-btn twrp-author-settings__js-author-remove-btn" type="button">
+							<span class="dashicons dashicons-no"></span>
+						</button>
 					</div>
 				<?php endforeach; ?>
 			<?php endif; ?>
 		</div>
+		<?php
+	}
 
-		<input id="twrp-author-settings__js-author-search" type="text" placeholder="Search for Author" />
-		<button id="twrp-author-settings__js-author-add-btn" type="button">Add Author</button>
+	/**
+	 * Display the search for authors, the add button, and a hidden input field
+	 * that remembers the authors.
+	 *
+	 * @param array $current_setting
+	 * @return void
+	 */
+	protected function display_add_authors_to_list( $current_setting ) {
+		?>
 		<input
-			id="twrp-author-settings__js-author-ids"
-			type="text"
-			name="<?= esc_attr( $this->get_setting_name() . '[authors]' ) ?>"
-			value="<?= esc_attr( $current_setting['authors'] ) ?>"
+			id="twrp-author-settings__js-author-search" type="text"
+			class="twrp-author-settings__author-search"
+			placeholder="<?= _x( 'Search for Author', 'backend', 'twrp' ); ?>"
+		/>
+
+		<button
+			id="twrp-author-settings__js-author-add-btn" type="button"
+			class="twrp-author-settings__js-author-add-btn button button-primary"
+		>
+			<?= _x( 'Add Author', 'backend', 'twrp' ); ?>
+		</button>
+
+		<input
+			id="twrp-author-settings__js-author-ids" type="text"
+			name="<?= esc_attr( $this->get_setting_name() . '[' . self::AUTHORS_IDS__SETTING_NAME . ']' ) ?>"
+			value="<?= esc_attr( $current_setting[ self::AUTHORS_IDS__SETTING_NAME ] ) ?>"
 		/>
 		<?php
 	}
