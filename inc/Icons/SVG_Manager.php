1f86a7a161321daf29124539f59ccd8869b33b86
9brada6 <9brada6@users.noreply.github.com>
2020-09-20 18:14:07 +0300

    Refactoring something...

--- inc/Icons/SVG_Manager.php
+++ inc/Icons/SVG_Manager.php
@@ -23,6 +23,25 @@ use TWRP\Icons\Rating_Icons;
 class SVG_Manager {
 
 	/**
+	 * Called before anything else, to initialize all the things that this class
+	 * needs.
+	 *
+	 * Always called at 'after_setup_theme' action. Other things added here should be
+	 * additionally checked, for example by admin hooks, or whether or not to be
+	 * included in special pages, ...etc.
+	 *
+	 * @return void
+	 */
+	public static function init() {
+		add_action( 'admin_head', array( __CLASS__, 'include_all_icons_file' ) );
+
+		// todo: remove:
+		add_action( 'admin_footer', array( __CLASS__, 'write_all_needed_icons_to_file' ) );
+	}
+
+	#region -- Get specific icon sets
+
+	/**
 	 * Get all registered icons that represents the user.
 	 *
 	 * @return array<string,array>
@@ -69,7 +88,6 @@ class SVG_Manager {
 		return Comments_Disabled_Icons::get_disabled_comment_icons();
 	}
 
-
 	/**
 	 * Get all registered icons that represents the number of views.
 	 *
@@ -97,6 +115,8 @@ class SVG_Manager {
 		return Rating_Icons::get_rating_packs();
 	}
 
+	#endregion -- Get specific icon sets
+
 	/**
 	 * Get all registered icons that represents the rating.
 	 *
@@ -105,88 +125,6 @@ class SVG_Manager {
 	public static function get_all_icons() {
 		return self::get_user_icons() + self::get_date_icons() + self::get_category_icons() +
 		self::get_comment_icons() + self::get_views_icons() + self::get_rating_icons();
-	}
-
-	/**
-	 * Called before anything else, to initialize all the things that this class
-	 * needs.
-	 *
-	 * Always called at 'after_setup_theme' action. Other things added here should be
-	 * additionally checked, for example by admin hooks, or whether or not to be
-	 * included in special pages, ...etc.
-	 *
-	 * @return void
-	 */
-	public static function init() {
-		add_action( 'admin_head', array( __CLASS__, 'include_all_icons_file' ) );
-
-		// todo: remove:
-		add_action( 'admin_footer', array( __CLASS__, 'write_all_needed_icons_to_file' ) );
-	}
-
-	/**
-	 * Creates a useful description for a icon, composed from the
-	 * brand(optionally), icon type and icon description.
-	 *
-	 * @param array $icon An icon type.
-	 * @param bool $with_brand Whether or not to include the brand, default false.
-	 * @return string
-	 */
-	public static function get_icon_description( $icon, $with_brand = false ) {
-		// todo:
-		return $icon['description'];
-	}
-
-	/**
-	 * Make the icons of same brand be nested in an array, where the key is the
-	 * brand name.
-	 *
-	 * @param array $icons
-	 * @return array
-	 */
-	public static function nest_icons_by_brands( $icons ) {
-		$branded_icons = array();
-
-		foreach ( $icons as $icon_id => $icon ) {
-			$brand = $icon['brand'];
-
-			if ( ! isset( $branded_icons[ $brand ] ) ) {
-				$branded_icons[ $brand ] = array();
-			}
-
-			$branded_icons[ $brand ][ $icon_id ] = $icon;
-		}
-
-		return $branded_icons;
-	}
-
-	/**
-	 * Echo all the svg defs.
-	 *
-	 * @return void
-	 */
-	public static function include_all_svg() {
-		$svg = self::get_all_vector_defs();
-		echo '<svg style="display:none;"><defs>' . $svg . '</defs></svg>'; // phpcs:ignore -- No XSS;
-	}
-
-	/**
-	 * Get a HTML string that will define all vectors.
-	 *
-	 * @return string
-	 */
-	public static function get_all_vector_defs() {
-		$output = '';
-
-		foreach ( self::get_date_icons() as $svg ) {
-			$output .= $svg['svg'];
-		}
-
-		foreach ( self::get_views_icons() as $svg ) {
-			$output .= $svg['svg'];
-		}
-
-		return $output;
 	}
 
 	/**
@@ -218,38 +156,6 @@ class SVG_Manager {
 		}
 
 		return '';
-	}
-
-	/**
-	 * @todo
-	 *
-	 * @return void
-	 */
-	public static function test_icons() {
-		echo '<script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>';
-
-		$icons = self::get_all_icons();
-
-		echo '<p>';
-		foreach ( $icons as $id => $vector ) {
-			echo "<span style='display:none'>";
-			echo $vector['svg']; // phpcs:ignore
-			echo '</span>';
-		}
-		foreach ( $icons as $id => $vector ) {
-			echo '<span style="display:block;margin:3px">';
-			self::include_svg( $id );
-			echo 'Ok <span style="margin-left: 6px;"> <span class="twrp-i twrp-views-i"><svg><use href="#twrp-views-goo-f" /></svg></span>Lala</span>';
-			echo '</span>';
-		}
-		?>
-
-		<?php
-		echo '</p>';
-	}
-
-	public static function include_svg( $icon_name, $additional_class = '' ) {
-		echo self::get_html_svg( $icon_name, $additional_class = '' );
 	}
 
 	public static function get_html_svg( $icon_name, $additional_class = '' ) {
@@ -416,7 +322,7 @@ class SVG_Manager {
 
 	#endregion -- Create needed icons, inline and in a file
 
-	#region -- Get All Used Icons Ids
+	#region -- Get all used website icons Ids
 
 	/**
 	 * Get an array with all used icons ids, for all widgets.
@@ -464,6 +370,78 @@ class SVG_Manager {
 		return array();
 	}
 
-	#endregion -- Get All Used Icons Ids
+	#endregion -- Get all used website icons Ids
+
+	#region -- Helpers
+
+	/**
+	 * Make the icons of same brand be nested in an array, where the key is the
+	 * brand name.
+	 *
+	 * @param array $icons
+	 * @return array
+	 */
+	public static function nest_icons_by_brands( $icons ) {
+		$branded_icons = array();
+
+		foreach ( $icons as $icon_id => $icon ) {
+			$brand = $icon['brand'];
+
+			if ( ! isset( $branded_icons[ $brand ] ) ) {
+				$branded_icons[ $brand ] = array();
+			}
+
+			$branded_icons[ $brand ][ $icon_id ] = $icon;
+		}
+
+		return $branded_icons;
+	}
+
+	/**
+	 * Creates a useful description for a icon, composed from the
+	 * brand(optionally), icon type and icon description.
+	 *
+	 * @param array $icon An icon type.
+	 * @param bool $with_brand Whether or not to include the brand, default false.
+	 * @return string
+	 */
+	public static function get_icon_description( $icon, $with_brand = false ) {
+		// todo:
+		return $icon['description'];
+	}
+
+	#endregion -- Helpers
+
+	#region -- Testing
+
+	/**
+	 * This function was created for testing and prototyping. It is not for use
+	 * in
+	 *
+	 * @return void
+	 */
+	public static function test_icons() {
+
+		$icons = self::get_all_icons();
+
+		echo '<p>';
+		foreach ( $icons as $id => $vector ) {
+			echo "<span style='display:none'>";
+			echo $vector['svg']; // phpcs:ignore
+			echo '</span>';
+		}
+		foreach ( $icons as $id => $vector ) {
+			echo '<span style="display:block;margin:3px">';
+			self::get_html_svg( $id );
+			echo 'Ok <span style="margin-left: 6px;"> <span class="twrp-i twrp-views-i"><svg><use href="#twrp-views-goo-f" /></svg></span>Lala</span>';
+			echo '</span>';
+		}
+		?>
+
+		<?php
+		echo '</p>';
+	}
+
+	#endregion -- Testing
 
 }
